a9201b563fd49b04939d130972c09b38e67d8833
ojferro <ojferro@gmail.com>
2024-03-27 11:33:03 -0700

    Working, cleanup

--- simulator.py
+++ simulator.py
@@ -4,7 +4,6 @@ import mujoco
 import mujoco.viewer
 
 import numpy as np
-import math
 import matplotlib.pyplot as plt
 
 np.set_printoptions(suppress=True)
@@ -33,10 +32,7 @@ def quaternion_to_euler(q):
 
 class LQR:
     # K = np.array([[-1,	-1.5512,	6.9235,	1.5845]])
-    # K = np.array([[-0.31622777, 46.18915178, -1.35648665, 13.04275023]])
-    # K = np.array([[-1.        , 52.27411189, -2.62866594, 14.99104346]])
     K = np.array([[-31.6227766 , 162.92827674, -30.40599676,  50.15742702]])
-    # K = np.array([[1.        , 1.43366053, 3.54176195, 0.64269921]])
 
     dt = 0.001
     prev_time = time.time()
@@ -47,15 +43,13 @@ class LQR:
         # For future reference: One of the things that was likely wrong was that the accelerometer to angle computation did not account for the linear accelerations
         # As such, the "angle" that I would compute would be wrong
 
-
-        # TODO Next time. Verify that this sensor data is correct.
         x = -position_sensor.data[0]
         x_dot = -velocity_sensor.data[0]
         theta = angle_sensor
         theta_dot = ang_velocity_sensor.data[1]
 
 
-        # For future reference, one of the things that was wrong was that the order of the state vars here vs in the LQR solver was difference...
+        # For future reference, one of the things that was wrong was that the order of the state vars here vs in the LQR solver was different...
 
         return np.array([x, theta, x_dot, theta_dot])
         # return np.array([x, x_dot, theta, theta_dot])
@@ -67,9 +61,6 @@ class LQR:
         state = self.compute_state(angle_sensor, position_sensor, velocity_sensor, ang_velocity_sensor)
         self.state_dbg = state
         # u is a force
-
-        # ordered_state = np.array([state[0], state[2], -state[1], state[3]])
-        # u = np.matmul(self.K, ordered_state)
         u = np.matmul(self.K, state)
 
         # convert u to torque, given wheel radius
@@ -77,15 +68,7 @@ class LQR:
         uT = u*r
         
         # Send half the torque to each wheel
-        # uT /= 2.0
-
-        # print("State = ", state)
-        # print("K*state = ", self.K * state)
-        # print("u: ", uT)
-        d.ctrl[:] = uT
-        print(uT)
-        print(d.ctrl)
-        print("------------------------------\n")
+        d.ctrl[:] = uT/2
 
 
 
@@ -96,6 +79,7 @@ d = mujoco.MjData(m)
 
 controller = LQR()
 
+times_dbg = np.array([])
 qvels_dbg = np.array([])
 ctrls_dbg = np.array([])
 
@@ -105,11 +89,12 @@ x_dot_dbg = np.array([])
 theta_dot_dbg = np.array([])
 
 ctr = 0
+step_start = time.time_ns()
 with mujoco.viewer.launch_passive(m, d) as viewer:
-    while viewer.is_running():
+    sim_start_time = time.time()
+    while viewer.is_running() and d.time < 6.0:
 
         ctr += 1
-        step_start = time.time()
 
         mujoco.mj_step(m, d)
 
@@ -120,13 +105,7 @@ with mujoco.viewer.launch_passive(m, d) as viewer:
             print("15 deg angle exceeded. Terminating...")
             # break
 
-        # if ctr > 1000:
-        #     break
-
-        # time.sleep(0.1)
-
-        print(f"Angle: {angle*180/np.pi}")
-
+        times_dbg = np.append(times_dbg, d.time)
         qvels_dbg = np.append(qvels_dbg, d.qvel[-1])
         ctrls_dbg = np.append(ctrls_dbg, d.ctrl[0])
         x_dbg = np.append(x_dbg, controller.state_dbg[0])
@@ -142,10 +121,15 @@ with mujoco.viewer.launch_passive(m, d) as viewer:
         viewer.sync()
 
         # Rudimentary time keeping, will drift relative to wall clock.
-        time_until_next_step = m.opt.timestep - (time.time() - step_start)
+        time_until_next_step = m.opt.timestep*(10**9) - (time.time_ns() - step_start)
         if time_until_next_step > 0:
-            time.sleep(time_until_next_step)
+            time.sleep(time_until_next_step/(10**9))
 
+        step_start = time.time_ns()
+
+
+sim_end_time = time.time()
+print(f"Simulation time: {sim_end_time - sim_start_time}")
 
 
 # Visualize data at the end of the run
@@ -153,22 +137,22 @@ print("Showing plots...")
 fig, ax = plt.subplots(2, 3)
 stride = 1
 
-ax[0,0].plot(qvels_dbg[::stride])
+ax[0,0].plot(times_dbg[::stride], qvels_dbg[::stride])
 ax[0,0].legend(["Velocity"])
 
-ax[0,1].plot(ctrls_dbg[::stride])
+ax[0,1].plot(times_dbg[::stride], ctrls_dbg[::stride])
 ax[0,1].legend(["Control"])
 
-ax[0,2].plot(x_dbg[::stride])
+ax[0,2].plot(times_dbg[::stride], x_dbg[::stride])
 ax[0,2].legend(["X"])
 
-ax[1,0].plot(theta_dbg[::stride])
+ax[1,0].plot(times_dbg[::stride], theta_dbg[::stride])
 ax[1,0].legend(["Theta"])
 
-ax[1,1].plot(x_dot_dbg[::stride])
+ax[1,1].plot(times_dbg[::stride], x_dot_dbg[::stride])
 ax[1,1].legend(["X_dot"])
 
-ax[1,2].plot(theta_dot_dbg[::stride])
+ax[1,2].plot(times_dbg[::stride], theta_dot_dbg[::stride])
 ax[1,2].legend(["Theta_dot"])
 
 plt.show()
